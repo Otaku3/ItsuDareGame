ec2becf0c5721ced4e0e8cd435a7df39e6a8adcd
Otaku3 <Otakumi@m.cs.osakafu-u.ac.jp>
2018-06-04 11:48:14 +0900

    complete textbook

--- ItsuDareGame/ViewController.swift
+++ ItsuDareGame/ViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var itsuLabel: UILabel!
+    @IBOutlet var dokodeLabel: UILabel!
+    @IBOutlet var daregaLabel: UILabel!
+    @IBOutlet var doshitaLabel: UILabel!
+    
+    let itsuArray: [String] = ["一年前","一週間前","昨日","今日"]
+    let dokoArray: [String] = ["山の上で","アメリカで","学校で","クラスで"]
+    let daregaArray: [String] = ["僕が","大統領が","先生が","友達が"]
+    let doshitaArray: [String] = ["叫んだ","演説した","怒った","踊った"]
+    
+    var index: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +30,44 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func change(){
+        itsuLabel.text = itsuArray[index]
+        dokodeLabel.text = dokoArray[index]
+        daregaLabel.text = daregaArray[index]
+        doshitaLabel.text = doshitaArray[index]
+        
+        index += 1
+        if index > 3 {
+            index = 0
+        }
+    }
+    
+    @IBAction func reset(){
+        itsuLabel.text = "---"
+        dokodeLabel.text = "---"
+        daregaLabel.text = "---"
+        doshitaLabel.text = "---"
+        
+        index = 0
+    }
+    
+    @IBAction func random(){
+        let itsuindex = Int(arc4random_uniform(4))
+        let dokodeindex = Int(arc4random_uniform(4))
+        let daregaindex = Int(arc4random_uniform(4))
+        let doshitaindex = Int(arc4random_uniform(4))
+        
+        print("いつ： \(itsuindex)")
+        print("どこで： \(dokodeindex)")
+        print("だれが： \(daregaindex)")
+        print("どうした： \(doshitaindex)")
+        
+        itsuLabel.text = itsuArray[itsuindex]
+        dokodeLabel.text = dokoArray[dokodeindex]
+        daregaLabel.text = daregaArray[daregaindex]
+        doshitaLabel.text = doshitaArray[doshitaindex]
     }
 
 
